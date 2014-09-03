1736004cce4c49106dd82f27f01535959b8243bf
Diego <dgelsin1@jhu.edu>
2014-09-03 13:50:49 -0400

    lunch day 2 hw

---
+++ day2-lunch/py_lunch_q1.py
@@ -0,0 +1,14 @@
+#!/usr/bin.env python
+
+number_alignments_sam = "/Users/cmdb/data/day2/accepted_hits.sam"
+
+f = open(number_alignments_sam)
+
+n_al = 0
+while True:
+    line = f.readline()
+    if line == "":
+        break
+    else:
+        n_al = n_al + 1
+print n_al
---
+++ day2-lunch/py_lunch_q2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+number_alignments_sam = "/Users/cmdb/data/day2/accepted_hits.sam"
+
+f = open(number_alignments_sam)
+
+n_al = 0
+while True:
+    line = f.readline()
+    if "NM:i:0" in line:
+        n_al = n_al + 1
+print n_al
+
+
+     
+     
+
+
+
