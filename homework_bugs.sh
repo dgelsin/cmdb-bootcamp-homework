73c76c025c200f10bfd9b2d91b69157dfbb9ad1d
Diego <dgelsin1@jhu.edu>
2014-09-03 09:26:14 -0400

    learned some tricks like seq during the homework discussion and added it to my debug

--- homework_bugs.sh
+++ homework_bugs.sh
@@ -6,11 +6,6 @@
 
 echo "There are around 6 mistakes"
 
-ONE="893"
-TWO="903"
-THREE="905"
-FOUR="915"
-
 FASTQ_DIR=/Users/cmdb/data/fastq
 OUTPUT_DIR=/Users/cmdb/data/day1
 SAMPLE_PREFIX="SRR072"
@@ -22,7 +17,7 @@ CUFFLINKS_RES=/accepted_hits.bam
 
 CORES=4
 
-for i in $ONE $TWO $THREE $FOUR
+for i in {893..916} # can do seq 893 916 as well
 do
   echo fastqc $FASTQ_DIR/$SAMPLE_PREFIX$i\.fastq.gz -o $OUTPUT_DIR
   echo tophat -p $CORES -G $OUTPUT_DIR/$ANNOTATION.gff -o $OUTPUT_DIR/$INDEX_DIR --no-novel-juncs --segment-length 20 $GENOME_DIR/$ANNOTATION $FASTQ_DIR/$SAMPLE_PREFIX$i
