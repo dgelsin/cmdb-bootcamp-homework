fa95fc15e35effda2095f7f544d61f732d37301d
Diego <dgelsin1@jhu.edu>
2014-09-03 14:17:11 -0400

    figured out the right category to look at

--- day2-lunch/py_lunch_q3.py
+++ day2-lunch/py_lunch_q3.py
@@ -1,1 +1,16 @@
-#!/usr/bin.env python
+#!/usr/bin/env python
+
+number_alignments_sam = "/Users/cmdb/data/day2/accepted_hits.sam"
+
+f = open(number_alignments_sam)
+
+n_al = 0
+while True:
+    line = f.readline()
+    if "NH:i:1" in line:
+        n_al = n_al + 1
+    else: 
+        if line == "":
+            break
+        
+print n_al
